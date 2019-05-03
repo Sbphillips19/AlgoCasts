7b9ec54297382ff2443ecd6d6860038df8828e28
sbphillips19 <sbphillips19@gmail.com>
2019-05-02 22:58:06 -0700

    maxChar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,29 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const charMap = {};
+  let max = 0;
+  let maxChar = '';
+
+  // use of for arrays
+  for (let character of str) {
+    if (!charMap[character]) {
+      charMap[character] = 1;
+    } else {
+      charMap[character]++;
+    }
+  }
+
+  // use in for objects
+  for (let char in charMap) {
+    if (charMap[char] > max) {
+      max = charMap[char];
+      maxChar = char;
+    }
+  }
+
+  return maxChar;
+}
 
 module.exports = maxChar;
