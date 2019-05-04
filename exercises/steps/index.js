f2d398b75760f15b52081b51a62cd278e155fd5e
sbphillips19 <sbphillips19@gmail.com>
2019-05-04 08:21:08 -0700

    steps

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,55 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+//GRIDER SOLUTION 2 Recursion
+// first time steps(3) so it doesn't return right away
+// since default row to 0
+function steps(n, row = 0, stair = '') {
+  if (n === row) {
+    return;
+  }
+
+  if (n === stair.length) {
+    console.log(stair);
+    // increment row by one since
+    // finished last step
+    // want empty stair again so rely
+    // on original function/ default value
+    return steps(n, row + 1);
+  }
+
+  if (stair.length <= row) {
+    stair += '#';
+  } else {
+    stair += ' ';
+  }
+  return steps(n, row, stair);
+}
 
 module.exports = steps;
+
+// my solution 1
+// function steps(n) {
+//   for (let i = 1; i <= n; i++) {
+//     let hashtag = '#';
+//     let space = ' ';
+//     let step = hashtag.repeat(i) + space.repeat(n - i);
+//     console.log(step);
+//   }
+// }
+
+// GRIDER solution 1
+
+// function steps(n) {
+//   for (let row = 0; row < n; row++) {
+//     let stair = '';
+//     for (let column = 0; column < n; column++) {
+//       if (column <= row) {
+//         stair += '#';
+//       } else {
+//         stair += ' ';
+//       }
+//     }
+//     console.log(stair);
+//   }
+// }
