5f2883169b5c95198597a4fd42342d7509e67660
sbphillips19 <sbphillips19@gmail.com>
2019-05-04 07:01:49 -0700

    anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,68 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+  return cleanString(stringA) === cleanString(stringB);
+}
+
+//helper function
+function cleanString(str) {
+  return str
+    .replace(/[^\w]/g, '')
+    .toLowerCase()
+    .split('')
+    .sort()
+    .join('');
+}
 
 module.exports = anagrams;
+
+// MY SOLUTION
+// function anagrams(stringA, stringB) {
+//   let cleanStringA = stringA.replace('[/^w/g]', '').toLowerCase();
+//   let cleanStringB = stringB.replace('[/^w/g]', '').toLowerCase();
+//
+//   if (
+//     cleanStringA
+//       .split('')
+//       .sort()
+//       .join('') ===
+//     cleanStringB
+//       .split('')
+//       .sort()
+//       .join('')
+//   ) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+// GRIDERS SOLUTION #1
+// create a character map and then compare the 2
+
+// function anagrams(stringA, stringB) {
+//   const aCharMap = buildCharMap(stringA);
+//   const bCharMap = buildCharMap(stringB);
+//
+//   if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+//     return false;
+//   }
+//
+//   for (let char in aCharMap) {
+//     if (aCharMap[char] !== bCharMap[char]) {
+//       return false;
+//     } else {
+//       return true;
+//     }
+//   }
+// }
+//
+// //create a helper function rather than 2 character maps
+// function buildCharMap(str) {
+//   const charMap = {};
+//   for (let char of str.replace(/[^\w]/g, '').toLowerCase()) {
+//     charMap[char] = charMap[char] + 1 || 1;
+//   }
+//   return charMap;
+// }
