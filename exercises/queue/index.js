313dfc460ac173f4899bc86b120f8d4c5adbf8d0
sbphillips19 <sbphillips19@gmail.com>
2019-05-05 09:38:31 -0700

    queue

--- exercises/queue/index.js
+++ exercises/queue/index.js
@@ -17,6 +17,32 @@
 // ADD- enqueuing or adding a record
 // REMOVE- dequeuing or removing a record
 
-class Queue {}
+// FIFO- first in first about
+
+// add to queue- array.unshift()
+// remove from queue- array.pop()
+
+class Queue {
+  // constructor called automatically when create new instance
+  constructor() {
+    this.data = [];
+  }
+
+  add(record) {
+    this.data.unshift(record);
+  }
+
+  remove() {
+    return this.data.pop();
+  }
+}
 
 module.exports = Queue;
+
+// The push() method adds one or more elements to the end of an array and returns the new length of the array.
+//
+// The shift() method removes the first element from an array and returns that removed element. This method changes the length of the array.
+//
+// The unshift() method adds one or more elements to the beginning of an array and returns the new length of the array.
+//
+// The pop() method removes the last element from an array and returns that element. This method changes the length of the array.
