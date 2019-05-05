653b155464c0c3894ff469a06c6974a3776db2f2
sbphillips19 <sbphillips19@gmail.com>
2019-05-05 10:02:13 -0700

    implementing a stack

--- exercises/stack/index.js
+++ exercises/stack/index.js
@@ -10,6 +10,30 @@
 //   s.pop(); // returns 2
 //   s.pop(); // returns 1
 
-class Stack {}
+// stack still ordered list of records
+// push new record on a stack, pop off a stack after
+// FILO- first in last out
+// LIFO- last in first out
+
+class Stack {
+  constructor() {
+    this.data = [];
+  }
+
+  // add to the top
+  push(record) {
+    this.data.push(record);
+  }
+
+  // remove top
+  pop() {
+    return this.data.pop();
+  }
+
+  //return top record without popping
+  peek() {
+    return this.data[this.data.length - 1];
+  }
+}
 
 module.exports = Stack;
